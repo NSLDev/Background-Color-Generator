b3977a5c0cab9fbf29de779e2d551c9bfeb47ad3
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2020-08-11 12:20:21 -0700

    Deleted redundant css files -Set up hooks for text, colors and background color -Background now responds to value of color selector

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,46 @@ import React, { useState } from "react";
 import "./App.scss";
 
 const App = () => {
-  const [colorText, setColorText] = useState("");
+  const [colorText, setColorText] = useState("Change the gradient");
+  const [color1Value, setColor1Value] = useState("#fc5c7d");
+  const [color2Value, setColor2Value] = useState("#6a82fb");
+  const [background, setBackground] = useState(
+    `linear-gradient(to right, ${color1Value}, ${color2Value})`
+  );
 
-  const getRandomColors = () => {};
+  const color1Change = (e) => {
+    setColor1Value(e.target.value);
+    setBackground(`linear-gradient(to right, ${color1Value}, ${color2Value})`);
+  };
+
+  const color2Change = (e) => {
+    setColor2Value(e.target.value);
+    setBackground(`linear-gradient(to right, ${color1Value}, ${color2Value})`);
+  };
+
+  // const changeColorText = () => {
+  //   setColorText(`Linear-gradient to right: ${color1Value} / ${color2Value}`);
+  // };
 
   return (
-    <div className="app">
+    <div className="app" style={{ background: background }}>
       <h1 className="app__heading">Background Generator</h1>
-      <input
-        type="color"
-        name="color1"
-        value="#FC5C7D"
-        className="app__color app__color--1"
-      />
-      <input
-        type="color"
-        name="color2"
-        value="#6A82FB"
-        className="app__color app__color--2"
-      />
+      <div className="app__colorContainer">
+        <input
+          type="color"
+          name="color1"
+          value={color1Value}
+          onChange={color1Change}
+          className="app__color app__color--1"
+        />
+        <input
+          type="color"
+          name="color2"
+          value={color2Value}
+          onChange={color2Change}
+          className="app__color app__color--2"
+        />
+      </div>
       <span className="app__colorText">{colorText}</span>
     </div>
   );
