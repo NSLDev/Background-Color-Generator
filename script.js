fed2e1f5cf1128ccd377d04ea80a037e9a1567b4
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-03-31 18:08:47 -0700

    -added script

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.querySelectorAll(".mygradient__input");
+const body = document.querySelector(".mygradient");
+body.style.background = "linear-gradient(to right, #BE93C5, #7BC6CC)";
+const inputs = document.querySelectorAll("mygradient__input");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const cssText = document.querySelector(".mygradient__css");
+
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  cssText.innerText = body.style.background;
+  console.log(color1, color2);
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
