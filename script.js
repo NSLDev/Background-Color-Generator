4e1aba3ade818a26e994119a7d8f4eed352ecf11
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-04-01 17:50:25 -0700

    -introduced beginner logic for gradient direction change

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const cssText = document.querySelector(".mygradient__css");
 const randomButton = document.querySelector(".mygradient__random");
+const directionButton = document.querySelector(".mygradient__direction");
 
 color1.value = "#C1929D";
 color2.value = "#767BA3";
+let direction = "to right";
+let clicked = false;
 setColors();
 
 function setColors() {
-  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  body.style.background = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
 }
 
 function setGradient() {
@@ -24,6 +27,12 @@ function setRandomGradient() {
   setGradient();
 }
 
+function changeDirection() {
+  direction = "to left";
+  setColors();
+}
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 randomButton.addEventListener("click", setRandomGradient);
+directionButton.addEventListener("click", changeDirection);
