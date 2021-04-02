98a6ee6a69982990997d1a6fd659de93b481e706
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-04-01 17:33:20 -0700

    -added in random color button

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const body = document.querySelector(".mygradient");
-body.style.background = "linear-gradient(to right, #BE93C5, #7BC6CC)";
-const inputs = document.querySelectorAll("mygradient__input");
+const colorInputs = document.querySelectorAll("mygradient__input");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const cssText = document.querySelector(".mygradient__css");
+const randomButton = document.querySelector(".mygradient__random");
+
+color1.value = "#C1929D";
+color2.value = "#767BA3";
+setColors();
+
+function setColors() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
 
 function setGradient() {
-  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  setColors();
   cssText.innerText = body.style.background;
-  console.log(color1, color2);
+}
+
+function setRandomGradient() {
+  color1.value = "#" + Math.random().toString(16).substr(-6);
+  color2.value = "#" + Math.random().toString(16).substr(-6);
+  setGradient();
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click", setRandomGradient);
