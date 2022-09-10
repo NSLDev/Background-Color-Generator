11dfeb3e3820196195ee14072c4d8e64f240f9da
Nicholas Lal <nlalkta@gmail.com>
2022-09-10 17:38:41 -0600

    -cleanup

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
-const body = document.querySelector(".mygradient");
+const body = document.getElementById("mygradient");
 const colorInputs = document.querySelectorAll("mygradient__input");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-const cssText = document.querySelector(".mygradient__css");
-const randomButton = document.querySelector(".mygradient__random");
-const directionButton = document.querySelector(".mygradient__direction");
+const cssText = document.getElementById("mygradient__css");
+const randomButton = document.getElementById("mygradient__random");
+const directionButton = document.getElementById("mygradient__direction");
 
-color1.value = "#C1929D";
-color2.value = "#767BA3";
-let direction = "to right";
-setColors(color1.value, color2.value);
 
-function setColors(color1, color2) {
-  body.style.background = `linear-gradient(${direction}, ${color1}, ${color2})`;
+color1.value = localStorage.getItem('backgroundColor').split(',')[1].trim() || "#C1929D";
+color2.value = localStorage.getItem('backgroundColor').split(',')[2].trim() || "#767BA3";
+
+const directions = ["to top", "to right", "to bottom", "to left"];
+let direction = 'to right';
+
+setColors();
+
+function copyToClipboard () {
+  navigator.clipboard.writeText(cssText.innerHTML);
+  alert(`${cssText.innerHTML} copied to clipboard!`);
 }
 
-function setGradient() {
-  setColors(color1.value, color2.value);
+function updateCSSText () {
   cssText.innerText = body.style.background;
+}
+
+function setColors() {
+  body.style.background = `linear-gradient(${direction}, ${color1.value}, ${color2.value})`;
+  updateCSSText();
+  localStorage.setItem('backgroundColor', `linear-gradient(${direction}, ${color1.value}, ${color2.value}`)
 }
 
 function setRandomGradient() {
-  color1.value = "#" + Math.random().toString(16).substr(-6);
-  color2.value = "#" + Math.random().toString(16).substr(-6);
-  setGradient();
+  color1.value = `#${Math.random().toString(16).substr(-6)}`;
+  color2.value = `#${Math.random().toString(16).substr(-6)}`;
+  setColors();
 }
 
 function changeDirection() {
-  const directions = ["to top", "to right", "to bottom", "to left"];
   direction = directions[Math.floor(Math.random() * directions.length)];
-  setColors(color1.value, color2.value);
-  cssText.innerText = body.style.background;
+  setColors();
 }
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+color1.addEventListener("input", setColors);
+color2.addEventListener("input", setColors);
 randomButton.addEventListener("click", setRandomGradient);
 directionButton.addEventListener("click", changeDirection);
+cssText.addEventListener("click", copyToClipboard);
